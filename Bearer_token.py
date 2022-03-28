66bad01c5faf96046a2414e8546f1196704947e0
pablo-jenne <61419450+pablo-jenne@users.noreply.github.com>
2022-03-28 10:12:37 +0200

    Add files via upload

--- Bearer_token.py
+++ Bearer_token.py
@@ -1,14 +1,23 @@
 import requests
 import json
 import time
-machines = ["000161228811", "161228811000", "000000000000", "111111111111"]
 
 
+test_json = '{ "SessionId":"0000650471314B9CBF4265A6D98CFFB459521B52E8A7553BA63C68CDCD76F4BB99", "Timeout":600}'
+
+machines_id = ["000161228811", "161228811000", "000000000000", "111111111111"]
+machines_ip = ["192.168.34.51", "192.168.34.52", "192.168.34.53", "192.168.34.54"]
+
+bearer_tokens = []
+ 
 while True:
 
-    for i in machines:
+    for i in range(len(machines_id)):
 
-        url = "https://192.168.34.51/Devices/" + i + "/profSession"
+        url = "https://" + machines_ip[i] +"/Devices/" + machines_id[i] + "/profSession"
+        print(url)
+
+
         payload = json.dumps({
             "loginName": "Admin",
             "Password": "Miele123"
@@ -17,27 +26,25 @@ while True:
           'Content-Type': 'application/json'
         }
 
-        response = requests.request("POST", url, headers=headers,verify=False, data=payload)
+        #response = requests.request("POST", url, headers=headers,verify=False, data=payload)
 
-        print(response.text)
+        x = json.loads(test_json) # test_json will be replaced by the json of response
+        x = x["SessionId"]
+        bearer_tokens.append(x)
+
+
+    #JSON DUMP
         time.sleep(1)
+    myjson = {
+        'Token_1': bearer_tokens[0],
+        'Token_2': bearer_tokens[1],
+        'Token_3': bearer_tokens[2],
+        'Token_4': bearer_tokens[3]
+    }
 
+    myjson = json.dumps(myjson)
+    print(myjson)
 
-
-
-
-
-
-
-payload = json.dumps({
-  "loginName": "Admin",
-  "Password": "Miele123"
-})
-headers = {
-  'Content-Type': 'application/json'
-}
-
-response = requests.request("POST", url, headers=headers,verify=False, data=payload)
-
-print(response.text)
-
+    with open("sample.json", "w") as outfile:
+        outfile.write(myjson)
+    time.sleep(540)
